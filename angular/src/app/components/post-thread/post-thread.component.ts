f7fd2b4d662348154450d0b99ba906bc0d7fc115
pitchapuay <wooncheek.dh@hotmail.com>
2020-06-12 21:44:24 +0700

    add component thread and post-thread

---
+++ angular/src/app/components/post-thread/post-thread.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-thread',
+  templateUrl: './post-thread.component.html',
+  styleUrls: ['./post-thread.component.css']
+})
+export class PostThreadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
