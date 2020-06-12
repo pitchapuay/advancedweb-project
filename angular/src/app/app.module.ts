7c1db953bda95341c56242daf1b3e87e8ffcffd3
pitchapuay <wooncheek.dh@hotmail.com>
2020-06-13 00:14:29 +0700

    edit signup

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -7,14 +7,18 @@ import { SignupComponent } from './components/user/signup/signup.component';
 import { SigninComponent } from './components/user/signin/signin.component';
 import { HomeComponent } from './components/home/home.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { ThreadComponent } from './components/thread/thread.component';
+import { PostThreadComponent } from './components/post-thread/post-thread.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     SigninComponent,
-    HomeComponent
+    HomeComponent,
+    ThreadComponent,
+    PostThreadComponent
   ],
   imports: [
     BrowserModule,
--- angular/src/app/components/user/signup/signup.component.ts
+++ angular/src/app/components/user/signup/signup.component.ts
@@ -9,17 +9,23 @@ import { FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
 export class SignupComponent implements OnInit {
 
   profileForm = new FormGroup({
-    title: new FormControl(''),
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+    username: new FormControl('',[Validators.required]),
+    title: new FormControl('',[Validators.required]),
+    firstName: new FormControl('',[Validators.required]),
+    lastName: new FormControl('',[Validators.required]),
     // stdid: new FormControl('',[Validators.required, Validators.pattern('B[0-9]{7}')]),
-    sex: new FormControl(''),
+    sex: new FormControl('',[Validators.required]),
     email: new FormControl('',[Validators.required, Validators.email]),
+    birthday: new FormGroup({
+      date: new FormControl('',[Validators.required]),
+      month: new FormControl('',[Validators.required]),
+      year: new FormControl('',[Validators.required])
+    }),
     address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
+      street: new FormControl('',[Validators.required]),
+      city: new FormControl('',[Validators.required]),
+      state: new FormControl('',[Validators.required]),
+      zip: new FormControl('',[Validators.required])
     }),
     aliases: new FormArray([
       new FormControl('')
